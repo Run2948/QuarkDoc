d8e9b86f03f6307e1ebb8fc4e3a2a2f3843dcde1
zhujinrun <15997352948@163.com>
2020-02-06 14:41:57 +0800

    恢复版本丢失的文件

---
+++ Mins.QuarkDoc.Framework/Log/LogHelper.cs
@@ -0,0 +1,22 @@
+﻿namespace Mins.QuarkDoc.Framework
+{
+    public class LogHelper
+    {
+        /// <summary>
+        /// 记录调试信息
+        /// </summary>
+        /// <param name="logStructure"></param>
+        public static void WriteInfoLog(string message)
+        {
+
+        }
+        /// <summary>
+        /// 记录报错日志
+        /// </summary>
+        /// <param name="logStructure"></param>
+        public static void WriteErrorLog(System.Exception exception)
+        {
+
+        }
+    }
+}
---
+++ Mins.QuarkDoc.Framework/Log/LogStructure.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mins.QuarkDoc.Framework
+{
+    public class LogStructure
+    {
+        public string Message { get; set; }
+        public string Exception { get; set; }
+        public DateTime CreateTime { get; set; }
+
+    }
+}
